d990a580649c404b4d62e75708d1cb3e9c4f1902
Michał Rosik <michalrosik1999@wp.pl>
2021-01-11 16:42:24 +0100

    Filling a function (KeyIsDown/ KeyIsUp/ RestartGame/

--- F16 game/Form1.cs
+++ F16 game/Form1.cs
@@ -34,16 +34,62 @@ namespace F16_game
         }
 
         private void KeyIsDown(object sender, KeyEventArgs e)
+
         {
-                
+            if (e.KeyCode == Keys.Up)
+            {
+                goUp = true;
+
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                goDown = true;
+            }
+            if (e.KeyCode == Keys.Space && shot == false)
+            {
+                MakeBullet();
+                shot = true;
+
+            }
         }
+
 
         private void KeyIsUp(object sender, KeyEventArgs e)
         {
 
+            if (e.KeyCode == Keys.Up)
+            {
+                goUp = false;
+
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                goDown = false;
+            }
+            if (shot == false)
+            {
+
+                shot = true;
+            }
+            if (e.KeyCode == Keys.Enter && gameOver == true)
+            {
+                RestartGame();
+            }
         }
+                
         private void RestartGame() 
         {
+            goUP = false;
+            goDown = false;
+            shot = false;
+            gameOver = false;
+            score = 0;
+            speed = 8;
+            UFOspeed = 10;
+
+            txtScore.Text = "Score:  " + score;
+
+            ChangeUFO();
 
 
         }
