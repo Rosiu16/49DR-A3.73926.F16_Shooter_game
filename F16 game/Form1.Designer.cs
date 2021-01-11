0124926943ccf2a3117e54496bc52a2208f7d830
Michał Rosik <michalrosik1999@wp.pl>
2021-01-11 16:15:13 +0100

    Add Pictures, Timer, Score

--- F16 game/Form1.Designer.cs
+++ F16 game/Form1.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace F16_game
 {
-    partial class Form1
+    partial class F16
     {
         /// <summary>
         /// Wymagana zmienna projektanta.
@@ -30,12 +30,107 @@ namespace F16_game
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(F16));
+            this.pillar1 = new System.Windows.Forms.PictureBox();
+            this.pillar2 = new System.Windows.Forms.PictureBox();
+            this.Player1 = new System.Windows.Forms.PictureBox();
+            this.ufo = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.Form1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pillar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pillar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Player1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ufo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pillar1
+            // 
+            this.pillar1.Image = ((System.Drawing.Image)(resources.GetObject("pillar1.Image")));
+            this.pillar1.Location = new System.Drawing.Point(600, -32);
+            this.pillar1.Name = "pillar1";
+            this.pillar1.Size = new System.Drawing.Size(75, 232);
+            this.pillar1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pillar1.TabIndex = 0;
+            this.pillar1.TabStop = false;
+            // 
+            // pillar2
+            // 
+            this.pillar2.Image = ((System.Drawing.Image)(resources.GetObject("pillar2.Image")));
+            this.pillar2.Location = new System.Drawing.Point(273, 265);
+            this.pillar2.Name = "pillar2";
+            this.pillar2.Size = new System.Drawing.Size(75, 232);
+            this.pillar2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pillar2.TabIndex = 1;
+            this.pillar2.TabStop = false;
+            // 
+            // Player1
+            // 
+            this.Player1.Image = ((System.Drawing.Image)(resources.GetObject("Player1.Image")));
+            this.Player1.Location = new System.Drawing.Point(62, 136);
+            this.Player1.Name = "Player1";
+            this.Player1.Size = new System.Drawing.Size(119, 100);
+            this.Player1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Player1.TabIndex = 2;
+            this.Player1.TabStop = false;
+            // 
+            // ufo
+            // 
+            this.ufo.Image = ((System.Drawing.Image)(resources.GetObject("ufo.Image")));
+            this.ufo.Location = new System.Drawing.Point(766, 219);
+            this.ufo.Name = "ufo";
+            this.ufo.Size = new System.Drawing.Size(68, 72);
+            this.ufo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.ufo.TabIndex = 3;
+            this.ufo.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 20;
+            this.timer1.Tick += new System.EventHandler(this.MainTimerEvent);
+            // 
+            // Form1
+            // 
+            this.Form1.AutoSize = true;
+            this.Form1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Form1.Location = new System.Drawing.Point(1, 0);
+            this.Form1.Name = "Form1";
+            this.Form1.Size = new System.Drawing.Size(110, 29);
+            this.Form1.TabIndex = 4;
+            this.Form1.Text = "Score: 0";
+            // 
+            // F16
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.SkyBlue;
+            this.ClientSize = new System.Drawing.Size(923, 470);
+            this.Controls.Add(this.Form1);
+            this.Controls.Add(this.ufo);
+            this.Controls.Add(this.Player1);
+            this.Controls.Add(this.pillar2);
+            this.Controls.Add(this.pillar1);
+            this.Name = "F16";
+            this.Text = "F16 Shooter Game";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyIsDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.KeyIsUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pillar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pillar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Player1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ufo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pillar1;
+        private System.Windows.Forms.PictureBox pillar2;
+        private System.Windows.Forms.PictureBox Player1;
+        private System.Windows.Forms.PictureBox ufo;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label Form1;
     }
 }
 
